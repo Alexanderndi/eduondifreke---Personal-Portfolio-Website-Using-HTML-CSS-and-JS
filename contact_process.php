dd800ff6bb35b24ad6243455b879615bc7ebd964
Ndifreke Eduo <64261470+Alexanderndi@users.noreply.github.com>
2022-01-02 21:18:08 +0100

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "eduondifreke@yahoo.com";
+    $to = "eduondifreke@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
